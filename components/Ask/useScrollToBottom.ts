e5c129d92ab8362912c71dc3384a4fe8e14331d8
Alex Gutierrez <algusaem@gmail.com>
2026-01-11 22:32:07 +0100

    Lint cleanup

--- components/Ask/useScrollToBottom.ts
+++ components/Ask/useScrollToBottom.ts
@@ -1,16 +1,18 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, DependencyList } from "react";
 
-export const useScrollToBottom = (dependencies: unknown[]) => {
+export const useScrollToBottom = (dependencies: DependencyList) => {
   const scrollContainerRef = useRef<HTMLDivElement>(null);
 
   const scrollToBottom = () => {
     if (scrollContainerRef.current) {
-      scrollContainerRef.current.scrollTop = scrollContainerRef.current.scrollHeight;
+      scrollContainerRef.current.scrollTop =
+        scrollContainerRef.current.scrollHeight;
     }
   };
 
   useEffect(() => {
     scrollToBottom();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, dependencies);
 
   return scrollContainerRef;
